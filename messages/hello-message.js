7722444fdd56130d3281364164ac973af8a2250d
Attiya <adarensburg@live.com>
2022-02-08 11:09:08 -0600

    first exercise

--- messages/hello-message.js
+++ messages/hello-message.js
@@ -1,1 +1,14 @@
 const helloMessage = "Greetings! Let us begin!";
+const {helloMessage} = require('./hello-message')
+// module.exports = helloMessage;
+// module.exports = {//you can add extra modules with this method using a comma separated list
+//     helloMessage
+// }
+
+function sayMessage(message){
+    console.log(`${message}`)
+}
+
+sayMessage(helloMessage);
+
+module.exports = {sayMessage}
